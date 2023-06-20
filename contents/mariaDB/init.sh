5e308d98aba12f97448faaf9ed6a311811e7cbf6
呂 博多 <blyu@ryonoMacBook-Air.local>
2023-06-20 10:46:03 +0900

    ...

--- contents/mariaDB/init.sh
+++ contents/mariaDB/init.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-mysqld_safe > /dev/null 2>&1 &
-if [ ! -d /var/lib/mysql/wordpress ]; then
+if [ -d /var/lib/mysql_buff ]; then
+    cp -rf /var/lib/mysql_buff/*  /var/lib/mysql/
+    rm -rf /var/lib/mysql_buff
+    mysqld_safe > /dev/null 2>&1 &
     mysql -u root -e "" > /dev/null 2>&1
     while [ $? -ne 0 ]; do
         mysql -u root -e "" > /dev/null 2>&1
     done
     mysql -u root -e "CREATE DATABASE wordpress;"
     mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'wordpress'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;"
+else
+    mysqld_safe > /dev/null 2>&1 &
 fi
 tail -f /var/log/mysql/mysql.log
