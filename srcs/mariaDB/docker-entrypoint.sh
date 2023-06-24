9bf21c7f4003e790eb0a42298fed78e3430715ae
呂 博多 <blyu@ryonoMacBook-Air.local>
2023-06-24 12:41:54 +0900

    ...

--- srcs/mariaDB/docker-entrypoint.sh
+++ srcs/mariaDB/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [ -d /var/lib/mysql_buff ]; then
+if [ ! -d /var/lib/mysql/$DB_NAME ]; then
     cp -rf /var/lib/mysql_buff/*  /var/lib/mysql/
-    rm -rf /var/lib/mysql_buff
-    chmod 777 /var/lib/mysql /var/lib/mysql/* /var/lib/mysql/*/* || true
     bash /setting.sh&
 fi
+if [ -d /var/lib/mysql_buff ]; then
+    rm -rf /var/lib/mysql_buff
+fi
+chmod 777 /var/lib/mysql /var/lib/mysql/* /var/lib/mysql/*/* || true
 exec mysqld
